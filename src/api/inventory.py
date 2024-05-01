ea1c497c18e082ad1c90cdb8bbe0b05ad265e2e3
justdanyuen <jdyuen@calpoly.edu>
2024-04-30 23:18:05 -0700

    removed some comments in audit

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -47,18 +47,6 @@ def get_inventory():
         "gold": total_gold
     }
 
-    # """ """
-    # with db.engine.begin() as connection:
-    #     potions = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory WHERE quantity > 0")).fetchall()
-    #     red_ml, green_ml, blue_ml, dark_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory")).one()
-    #     gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
-    #     total_ml = red_ml + green_ml + blue_ml + dark_ml
-    #     total_potions = 0
-    #     for potion in potions:
-    #         total_potions += potion.quantity
-    # print({"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": gold})
-    # return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": gold}
-
 # Gets called once a day
 @router.post("/plan")
 def get_capacity_plan():
