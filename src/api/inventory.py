38ca8762255fbdcece87cacbda96ba4d92cc604b
justdanyuen <jdyuen@calpoly.edu>
2024-05-13 07:23:28 -0700

    buy 5 pot cap, 2 ml cap

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -178,8 +178,8 @@ def get_capacity_plan():
             add_to_ml += 1
 
     return {
-        "potion_capacity": 2,
-        "ml_capacity": 5
+        "potion_capacity": 5,
+        "ml_capacity": 2
         }
 
 class CapacityPurchase(BaseModel):
