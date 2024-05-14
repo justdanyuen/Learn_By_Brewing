18f06be60813776eb0af4efb1f6aa88075251056
justdanyuen <jdyuen@calpoly.edu>
2024-05-14 14:01:22 -0700

    turned capacity plan OFF

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -177,10 +177,10 @@ def get_capacity_plan():
             total_cost -= 1000
             add_to_ml += 1
 
-    add_to_pot = 5
-    add_to_ml = 4
+    add_to_pot = 0
+    add_to_ml = 0
 
-    
+
     print(f"Adding {add_to_pot} capacities to potions, {add_to_ml} capacities to ml")
     return {
         "potion_capacity": add_to_pot,
